cf6b72f8fdeaeb99901fba812f88dd21f7ac08d4
Daniil Pavlenko <pavlenko.develop@gmail.com>
2020-02-02 13:44:41 +0300

    Added LogOut menu item

--- app/src/main/java/com/example/daggerexample/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/daggerexample/ui/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.daggerexample.ui.main
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.example.daggerexample.BaseActivity
 import com.example.daggerexample.R
@@ -18,5 +20,20 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, TAG, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> {
+                sessionManager.logOut()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
